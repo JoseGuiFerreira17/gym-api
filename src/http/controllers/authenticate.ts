f80b38170f13e9174b3986e6ba149b230e21b21b
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2025-01-23 17:43:07 -0300

    authentication scope

---
+++ src/http/controllers/authenticate.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod';
+import { FastifyRequest, FastifyReply } from 'fastify';
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
+import { AuthenticateService } from '@/services/authenticate';
+import { InvalidCredentialsError } from '@/services/erros/invalid-credentials';
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const { email, password } = z
+    .object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    })
+    .parse(request.body);
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository();
+    const registerService = new AuthenticateService(prismaUsersRepository);
+    await registerService.execute({ email, password });
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: err.message });
+    }
+    throw err;
+  }
+
+  return reply.status(200).send();
+}
