802b5f1fe5b71af0b696530355d8a0ccc083229b
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2025-01-28 15:39:14 -0300

    creation of end to end tests

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,29 @@
-import { test } from 'vitest';
+import request from 'supertest';
+import { app } from '@/app';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
-test('ok', () => {});
+describe('authenticate controller', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    });
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johndoe@email.com',
+      password: '123456',
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty('token');
+  });
+});
--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,23 @@
-import { test } from 'vitest';
+import request from 'supertest';
+import { app } from '@/app';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
-test('ok', () => {});
+describe('resgister controller', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to register a new user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    });
+
+    expect(response.statusCode).toBe(201);
+  });
+});
