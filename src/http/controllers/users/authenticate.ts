42e44cd4b272c043b58cbecd5112c0ca2fe628a3
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2025-01-29 14:23:15 -0300

    implementing token refresh

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -17,9 +17,22 @@ export async function authenticate(
   try {
     const registerService = makeAuthenticateService();
     const { user } = await registerService.execute({ email, password });
-    const token = await reply.jwtSign({}, { sign: { sub: user.id } });
 
-    return reply.status(200).send({ token });
+    const token = await reply.jwtSign({}, { sign: { sub: user.id } });
+    const refreshToken = await reply.jwtSign(
+      {},
+      { sign: { sub: user.id, expiresIn: '7d' } },
+    );
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({ token });
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: err.message });
